f5db397bfd9ff251fcc0b9630d147db92d39f3b1
Maria Usina <usinamaria2189@gmail.com>
2026-08-27 11:21:43 -0500

    feat(assignment10): verify reCAPTCHA token on registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const crypto = require("crypto");
 const { randomUUID } = crypto;
 const util = require("util");
 const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
 const { userSchema, logonSchema } = require("../validation/userSchema");
 const prisma = require("../db/prisma");
 
@@ -54,6 +55,40 @@ async function comparePassword(inputPassword, storedHash) {
  */
 async function register(req, res, next) {
   if (!req.body) req.body = {};
+
+  let isPerson = false;
+  if (req.body.recaptchaToken) {
+    const token = req.body.recaptchaToken;
+    const params = new URLSearchParams();
+    params.append("secret", process.env.RECAPTCHA_SECRET);
+    params.append("response", token);
+    params.append("remoteip", req.ip);
+    const response = await fetch(
+      // might throw an error that would cause a 500 from the error handler
+      "https://www.google.com/recaptcha/api/siteverify",
+      {
+        method: "POST",
+        body: params.toString(),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+    );
+    const data = await response.json();
+    if (data.success) isPerson = true;
+    delete req.body.recaptchaToken;
+  } else if (
+    process.env.RECAPTCHA_BYPASS &&
+    req.get("X-Recaptcha-Test") === process.env.RECAPTCHA_BYPASS
+  ) {
+    // might be a test environment
+    isPerson = true;
+  }
+  if (!isPerson) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Bot verification failed. Please complete the reCAPTCHA." });
+  }
 
   const { error, value } = userSchema.validate(req.body, {
     abortEarly: false,
